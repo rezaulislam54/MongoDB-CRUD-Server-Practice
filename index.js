8e8d2f7064fee595f4a1f3264e77e4e735c0718b
rezaulislam54 <rezaulislam5490@gmail.com>
2024-10-04 22:49:57 +0600

    user information update function added

--- index.js
+++ index.js
@@ -51,6 +51,23 @@ async function run() {
    })
 
 
+   app.put("/users/:id", async(req, res) =>{
+    const id = req.params.id;
+    const user = req.body;
+    const filter = {_id: new ObjectId(id)};
+    const options = { upsert: true };
+    const updatedUser = {
+      $set:{
+        name: user.name,
+        email: user.email
+      }
+    }
+    const result = await usersColection.updateOne(filter, updatedUser, options);
+    res.send(result);
+
+   })
+
+
    app.delete("/users/:id", async(req, res) =>{
     const id = req.params.id;
     const query = {_id: new ObjectId(id)}
