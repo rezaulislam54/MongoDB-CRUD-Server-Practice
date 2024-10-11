b39a2e347799636b2887d5a9715d73ec9b02da44
rezaulislam54 <rezaulislam5490@gmail.com>
2024-10-12 01:30:39 +0600

    some change

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -14,7 +15,7 @@ app.get("/", (req , res)=>{
 })
 
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hflxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hflxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
